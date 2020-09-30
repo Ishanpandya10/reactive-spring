443470e21a05dbc9b3aa733b646e6441b99fab27
Ishan Pandya <ishanpandya10@gmail.com>
2020-09-30 17:29:35 +0530

    Initial commit with dependencies

---
+++ src/test/java/com/webserviceserver/WebServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
