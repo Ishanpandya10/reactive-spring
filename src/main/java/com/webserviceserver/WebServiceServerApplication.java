16fda706a52dc4730bc0704ca9f7f009dea506bd
Ishan Pandya <ishanpandya10@gmail.com>
2020-09-30 18:26:57 +0530

    Adding http endpoint

--- src/main/java/com/webserviceserver/WebServiceServerApplication.java
+++ src/main/java/com/webserviceserver/WebServiceServerApplication.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class WebServiceServerApplication {
@@ -23,10 +29,12 @@ public class WebServiceServerApplication {
  */
 @RestController
 class Api {
-    @GetMapping("/message")
-    public ResponseEntity<WelcomeMessage> getMessage() {
-        WelcomeMessage welcomeMessage = new WelcomeMessage("asd", LocalDateTime.now());
-        return ResponseEntity.ok(welcomeMessage);
+    @GetMapping(value = "/message", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Flux<WelcomeMessage> getMessage() {
+        List<WelcomeMessage> welcomeMessages = Arrays.asList(new WelcomeMessage("asd", LocalDateTime.now())) ;
+        return Flux.fromStream(welcomeMessages.stream())
+                .limitRate(1)
+                .delayElements(Duration.ofSeconds(1));
     }
 }
 
